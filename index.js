03ee017b41527ac47b96ea98901516c88175ae88
Tomasz Dysinski <oz@bunnybones.com>
2014-12-23 23:49:26 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,67 @@
+function MultitargetFramer(camera, targetPoints, domSize) {
+	this.camera = camera;
+	this.targetPoints = targetPoints;
+	this.domSize = domSize;
+
+	this.targetAverage = new THREE.Vector3();
+	this.targetPointsTotal = this.targetPoints.length;
+
+	//
+	this.reframeZoomSpeed = .15,
+	this.reframeOffsetSpeed = .25,
+	this.frameMargin = new THREE.Vector2(.5, .5), 
+	
+	this._fullFrame = new THREE.Vector2(), 
+	this._zoom = 1,
+	this._halfFrame = new THREE.Vector2(), 
+	this._offset = new THREE.Vector2(), 
+	this._topLeft = new THREE.Vector2(), 
+	this._size = new THREE.Vector2(),
+	this._projector = new THREE.Projector(),
+	this._screenspaceBounds = new THREE.Box2(),
+	this._screenspaceTargetPoint = new THREE.Vector3(),
+	this._screenspaceSize,
+	this._screenspaceOffset;
+}
+
+MultitargetFramer.prototype = {
+	update: function() {
+		this.targetAverage.set(0, 0, 0);
+		for (var i = this.targetPointsTotal - 1; i >= 0; i--) {
+			this.targetAverage.add(this.targetPoints[i]);
+		};
+		this.targetAverage.multiplyScalar(1 / this.targetPointsTotal);
+		this.camera.lookAt(this.targetAverage);
+
+		this._screenspaceBounds.makeEmpty();
+		for (var i = this.targetPointsTotal - 1; i >= 0; i--) {
+			this._screenspaceTargetPoint.copy(this.targetPoints[i]);
+			this._projector.projectVector( this._screenspaceTargetPoint, this.camera );
+			this._screenspaceBounds.expandByPoint(this._screenspaceTargetPoint);
+		}
+		this._screenspaceBounds.expandByVector(this.frameMargin);
+		this._screenspaceSize = this._screenspaceBounds.size();
+		this._screenspaceOffset = this._screenspaceBounds.center();
+		this._offset.x += this._screenspaceOffset.x * this._fullFrame.x * .5 * this.reframeOffsetSpeed;
+		this._offset.y -= this._screenspaceOffset.y * this._fullFrame.y * .5 * this.reframeOffsetSpeed;
+		this._zoom -= (this._zoom - this._zoom * (this._screenspaceSize.x > this._screenspaceSize.y ? this._screenspaceSize.x : this._screenspaceSize.y) * .5) * this.reframeZoomSpeed;
+		this._zoom = Math.max(.1, Math.min(2, this._zoom));
+		this._offset.x = Math.max(-1000, Math.min(1000, this._offset.x));
+		this._offset.y = Math.max(-1000, Math.min(1000, this._offset.y));
+
+		this._fullFrame.copy(this.domSize);
+		this._halfFrame.copy(this._fullFrame).multiplyScalar(.5);
+		this._topLeft.copy(this._halfFrame).multiplyScalar(1-this._zoom).add(this._offset);
+		this._size.copy(this._fullFrame).multiplyScalar(this._zoom);
+		this.camera.setViewOffset(
+			this._fullFrame.x, 
+			this._fullFrame.y, 
+			this._topLeft.x,
+			this._topLeft.y,
+			this._size.x,
+			this._size.y
+		);
+	}
+}
+
+module.exports = MultitargetFramer;
---
+++ test.js
@@ -0,0 +1,56 @@
+var onReady = function() {
+	var View = require('threejs-managed-view').View;
+	var MultitargetFramer = require('./');
+	var view = new View({
+		useRafPolyfill: false
+	});
+	var scene = view.scene;
+
+	var sphereGeometry = new THREE.SphereGeometry(1.5);
+	var size = 500;
+	var sizeHalf = size * .5;
+	var bounds = new THREE.Box3(
+		new THREE.Vector3(-sizeHalf, -sizeHalf, -sizeHalf),
+		new THREE.Vector3(sizeHalf, sizeHalf, sizeHalf)
+	)
+	var random = new THREE.Vector3();
+	var boundSize = bounds.size();
+	for (var i = 0; i < 1200; i++) {
+		var ball = new THREE.Mesh(sphereGeometry);
+		scene.add(ball);
+		random.set(
+			Math.random(),
+			Math.random(),
+			Math.random()
+		);
+		ball.position.copy(bounds.min).add(random.multiply(boundSize));
+	};
+
+	var camera = view.camera;
+	var margin = 1;
+
+	var targetBoxGeometry = new THREE.BoxGeometry(30, 6, 6, 1, 1, 1);
+	var targetBoxMesh = new THREE.Mesh(targetBoxGeometry);
+	var targetPoints = targetBoxGeometry.vertices;
+	scene.add(targetBoxMesh);
+	var framingController = new MultitargetFramer(camera, targetPoints, view.domSize, margin);
+
+	var camSwayDistance = 100;
+	view.renderManager.onEnterFrame.add(function() {
+		var time = (new Date()).getTime() * .001 * .25;
+		camera.position.set(
+			Math.sin(time) * camSwayDistance,
+			Math.cos(time * 1.33) * camSwayDistance,
+			Math.sin(time * 3) * camSwayDistance + camSwayDistance
+		)
+		framingController.update();
+	})
+}
+
+var loadAndRunScripts = require('loadandrunscripts');
+loadAndRunScripts(
+	[
+		'bower_components/three.js/three.js'
+	],
+	onReady
+);
